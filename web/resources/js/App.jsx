0e87a0ea7c48a7ead4b45f779101f4072dcf8001
DESKTOP-B0D0J2Q\ADMIN <haodcdng@gmail.com>
2023-03-30 18:27:44 +0700

    useReducer

--- web/resources/js/App.jsx
+++ web/resources/js/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client'
 import ListItem from "./components/ListItem";
+import TestListUseReducer from "./components/TestListUseReducer";
 
 export default function App(){
     return(
         <div id="container-wrapper" className="pad-top-100 pad-bottom-100 parallax">
             <div className="container-fluid">
                 <div className="row">
+                    <TestListUseReducer />
+                     
+                </div>
+                <div className="row">
                     <ListItem />
-                            
+                     
                 </div>
                 {/* end row */}
                 
